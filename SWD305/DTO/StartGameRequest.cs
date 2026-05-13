cce4c168b8619a81b06e3dbe0241fbfba1c5f2c3
THINH <thinhnguyen123258@gmail.com>
2026-05-13 22:42:25 +0700

    First commit

--- SWD305/DTO/StartGameRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SWD305.DTO
-{
-    public class StartGameRequest
-    {
-        public int UserId { get; set; }
-        public int GameId { get; set; }
-    }
-
-}
---
+++ SWP392/Program.cs
@@ -0,0 +1,76 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models; 
+using SWD305.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<>(options =>
+//    options.UseSqlServer(
+//        builder.Configuration.GetConnectionString("DefaultConnection")
+//    ));
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler =
+            System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] {}
+    }
+});
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "SessionToken";
+    options.DefaultChallengeScheme = "SessionToken";
+}).AddScheme<Microsoft.AspNetCore.Authentication.AuthenticationSchemeOptions, SWD305.Security.SessionTokenAuthHandler>("SessionToken", null);
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
